224f707e51c42812b4b162070b1b08b587e06b92
Vigneshkumar <tkmigneshkumar@gmail.com>
2018-09-05 00:40:34 -0500

    mongo config

--- server/db/mongoosedb.js
+++ server/db/mongoosedb.js
@@ -2,9 +2,7 @@ const mongoose = require('mongoose')
 
 mongoose.promise = global.promise;
 
-/* mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost:27017/MernStackTest'); */
-
-mongoose.connect('mongodb://localhost:27017/MernStackTest')
+mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost:27017/MernStackTest'); 
 
 
 module.exports = {mongoose};
